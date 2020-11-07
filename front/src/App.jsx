26d00c2f5f390c48a969f51ccecde865926fd354
中村陸人 <>
2020-11-07 18:07:44 +0900

    Routerの設定

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,39 +1,23 @@
 import React, { useEffect,useState } from 'react';
 import './App.css';
-import json from './apis/json';
-import Article from './components/Article';
-
-// class App extends React.Component {
-
-//    constructor(props) {
-//     super(props);
-//     //const results = json.get('/articles')
-//     this.state = { articles: [] };
-//   }
-
-//   render() {
-//     return(
-//       <Article articles = {this.state.articles}/>
-//     )
-//   }
-// }
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import Top from './components/Top';
+import articleCreate from './components/articleCreate';
 
 const App = () =>{
-  const [articles, setArticles] = useState([]);
-
-  useEffect(() => {
-    const getArticles = async() => {
-      try {
-        const results = await json.get('/articles')
-        setArticles(results.data)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    getArticles()
-  }, [])
   return(
-    <Article articles = {articles}/>
+    <div className='container'>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Top} />
+          <Route path="/article/create/" exact component={articleCreate} />
+        </Switch>
+        <Link to="/article/create/">
+        <button className='create_btn'>+</button>
+        </Link>
+      </Router>
+    </div>
   )
 }
 
---
+++ front/src/components/articleCreate.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const articleCreate = ({}) =>{
+  return(
+    <>
+      <p>aaaa</p>
+    </>
+  )
+}
+
+export default articleCreate;
