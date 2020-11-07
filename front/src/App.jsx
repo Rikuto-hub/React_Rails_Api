7be991130320d49807e5ef4f4b4096fd2570d602
中村陸人 <>
2020-11-07 20:09:16 +0900

    Footerの追加

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,22 +3,21 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Top from './components/Top';
 import articleCreate from './components/articleCreate';
 
 const App = () =>{
   return(
     <Router>
-      <Header/>
+      <Header />
       <div className='container'>
         <Switch>
           <Route path="/" exact component={Top} />
           <Route path="/article/create/" exact component={articleCreate} />
         </Switch>
-        <Link to="/article/create/">
-        <button className='create_btn'>+</button>
-        </Link>
       </div>
+      <Footer />
     </Router>
   )
 }
