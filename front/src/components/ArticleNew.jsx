bd2f5f45e50f8146718b108944ea60676c24df31
中村陸人 <>
2020-11-08 13:04:34 +0900

    Createの値の取得を同期処理に変更 Create時のlinkを設定

--- front/src/components/ArticleNew.jsx
+++ front/src/components/ArticleNew.jsx
@@ -1,19 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import json from '../apis/json';
+import { Link } from 'react-router-dom';
+import '../styles/ArticleNew.css';
 
 const ArticleNew = ({}) =>{
-  const [name, titleArticle] = useState("");
-  const [price, numberArticle] = useState("");
-  const [content, contentArticle] = useState("");
-  // useEffect(() => {
-  // }, [])
 
   const postArticle = async() => {
+    const name = document.getElementById('articleName').value
+    const price = document.getElementById('articlePrice').value
+    const content = document.getElementById('articleContent').value
+
     try {
-      titleArticle(document.getElementById('articleName').value)
-      numberArticle(document.getElementById('articlePrice').value)
-      contentArticle(document.getElementById('articleContent').value)
-      
       await json.post('/articles', {
         article: {name: name, price: price, content: content}
       }) 
@@ -25,11 +22,21 @@ const ArticleNew = ({}) =>{
   return(
     <>
       <form>
-        <input type="text" id="articleName"/>
-        <input type="number" id="articlePrice"/>
-        <textarea id="articleContent" cols="30" rows="10" placeholder="詳細"></textarea>
+        <div className='article_new'>
+          <label htmlFor="articleName">Name</label>
+          <input type="text" id="articleName"/>
+        </div>
+        <div className='article_new'>
+          <label htmlFor="articlePrice">Price</label>
+          <input type="number" id="articlePrice"/>
+        </div>
+        <div className='article_new'>
+          <textarea id="articleContent" cols="40" rows="5" placeholder="詳細"></textarea>
+        </div>
       </form>
-      <button type="submit" id="articleSubmit" onClick={postArticle}>登録</button>
+      <Link to="/">
+      <button type="submit" id="articleSubmit" className="article_btn" onClick={postArticle}>登録</button>
+      </Link>
     </>
   )
 }
