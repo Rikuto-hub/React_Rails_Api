058c97d2017a4c0623fa481efedf2a94fce83329
中村陸人 <>
2020-12-25 06:47:30 +0900

    Registration.jsxのスタイルの修正

--- front/src/components/Registration.jsx
+++ front/src/components/Registration.jsx
@@ -1,32 +1,17 @@
-import React, { Component } from "react";
+import React, { useState } from 'react';
 import json from '../apis/json';
+import '../styles/Registration.css' 
 
-export default class Registration extends Component {
-  constructor(props) {
-    super(props);
+const Registration = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password_confirmation, setPassword_confirmation] = useState("");
 
-    this.state = {
-      name: "",
-      email: "",
-      password: "",
-      password_confirmation: "",
-      registrationErrors: ""
-    };
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
-
-  handleSubmit(event) {
-    const { name, email, password, password_confirmation } = this.state;
-
-    json
+  const onHandleSubmit = (event) =>{
+    event.preventDefault();
+    const onRegistrationSubmit = (name, email, password, password_confirmation) => {
+      json
       .post(
         "/users",
         {
@@ -41,59 +26,61 @@ export default class Registration extends Component {
       )
       .then(response => {
         if (response.data.status === "created") {
-          this.props.handleSuccessfulAuth(response.data);
+          console.log("registration");
         }
       })
       .catch(error => {
         console.log("registration error", error);
       });
-    event.preventDefault();
+    }
+    onRegistrationSubmit(name, email, password, password_confirmation)
   }
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
+  return(
+    <div　className="registration">
+      <p>ユーザー登録</p>
+      <form onSubmit={onHandleSubmit}>
 
-          <input
-            type="text"
-            name="name"
-            placeholder="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-            required
-          />
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          value={name}
+          onChange={(event)=>{setName(event.target.value)}}
+          required
+        />
 
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={this.state.email}
-            onChange={this.handleChange}
-            required
-          />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(event)=>{setEmail(event.target.value)}}
+          required
+        />
 
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={this.state.password}
-            onChange={this.handleChange}
-            required
-          />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={(event)=>{setPassword(event.target.value)}}
+          required
+        />
 
-          <input
-            type="password"
-            name="password_confirmation"
-            placeholder="Password confirmation"
-            value={this.state.password_confirmation}
-            onChange={this.handleChange}
-            required
-          />
+        <input
+          type="password"
+          name="password_confirmation"
+          placeholder="Password confirmation"
+          value={password_confirmation}
+          onChange={(event)=>{setPassword_confirmation(event.target.value)}}
+          required
+        />
 
-          <button type="submit">Register</button>
-        </form>
-      </div>
-    );
-  }
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  )
 }
+
+export default Registration;
