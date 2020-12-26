625db04c6d6e7b06aea24f66be8ad589cf279a2f
中村陸人 <>
2020-12-26 12:24:11 +0900

    CSRF対策の追加

--- front/src/apis/json.js
+++ front/src/apis/json.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 export default axios.create({
   baseURL: process.env.REACT_APP_DEV_API_URL,
   withCredentials: true,
-  // headers: {'X-Requested-With': 'XMLHttpRequest'}
+  headers: {'X-Requested-With': 'XMLHttpRequest'}
 })
