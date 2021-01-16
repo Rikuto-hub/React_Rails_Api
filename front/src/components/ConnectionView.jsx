319726327c2443e657c2ae5b1f773e1c002f1145
中村陸人 <>
2021-01-16 13:37:23 +0900

    バグの修正

--- front/src/components/ConnectionView.jsx
+++ front/src/components/ConnectionView.jsx
@@ -6,12 +6,14 @@ const ConnectionView = ({connections}) =>{
   const connection = connections.map(connection => {
     return(
     <div key={connection.id}>
+      {connection.avatar &&(
       <div className='connectionUser'>
         <Link to = {{ pathname: '/account', state: {user_id: connection.user_id} }}>
           <img src={connection.avatar} alt=""/>
           <p>{connection.user_name}</p>
         </Link>
       </div>
+      )}
       <a href={connection.url} target="_Blank" rel="noopener noreferrer" className='connectionTop'>
         <div className='connectionItem' >
           <p>{connection.content}</p>
