7b1233f8f14a104a42183c75c269abd4f806c8dd
中村陸人 <>
2021-01-17 22:10:53 +0900

    SkeletonScreenの作成

--- front/src/components/profile/ProfileTop.jsx
+++ front/src/components/profile/ProfileTop.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Avatar from '../../images/avatar.jpeg';
+import Skeleton from 'react-loading-skeleton';
+import { Img } from 'react-image';
 
 const ProfileTop = ({profile, props}) => {
   return(
     <div className='profileTop'>
       {profile ? (
-        <img src={profile.avatar} alt='画像がありません' />) : (
-        <img src={Avatar} alt="画像がありません"/>
+        <Img src={profile.avatar}
+          loader={<Skeleton width={100} height={100} />}
+          unloader={<Skeleton width={100} height={100} />}
+        />
+      ) : (
+        <Skeleton width={100} height={100} />
       )}
       {profile ?(
         <p>{profile.name}</p>) : (
